0b5c9168f293fe3aeab999170e8530f437788619
happiesly <hudonte28@gmail.com>
2022-09-02 13:29:14 +0100

    Updated the HTML,CSS and JS files

---
+++ script.js
@@ -0,0 +1,67 @@
+function displayDate(dayTime) {
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = currentDate.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayArray = days[day];
+  return `${dayArray} ${hours}:${minutes}`;
+}
+function showWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#degree").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#sky").innerHTML = response.data.weather[0].main;
+  document.querySelector("#humid").innerHTML = response.data.main.humidity;
+  document.querySelector("#windy").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function search(city) {
+  let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+function handleSub(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);
+}
+
+function findLocation(position) {
+  let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findLocation);
+}
+let dateId = document.querySelector("#date");
+let currentDate = new Date();
+dateId.innerHTML = displayDate(currentDate);
+
+let searchId = document.querySelector("#search");
+searchId.addEventListener("submit", handleSub);
+
+let currentLocation = document.querySelector("#current-button");
+currentLocation.addEventListener("click", getCurrentLocation);
+
+search("uyo");
